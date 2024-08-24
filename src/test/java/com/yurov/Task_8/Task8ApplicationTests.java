092290e549d901238df5bcc165b8580402540c34
Yurov Pavel <sendmeof@gmail.com>
2024-08-24 03:48:48 +0300

    Init commit

---
+++ src/test/java/com/yurov/Task_8/Task8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurov.Task_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
